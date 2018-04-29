29320921f2faad898bcc200089f844039030e3a4
Milan Timko <milan.timko@akademiasovy.sk>
2018-04-29 21:34:57 +0200

    Fixed merge

--- src/containers/BigAdvert/index.js
+++ src/containers/BigAdvert/index.js
@@ -1,113 +1,15 @@
-<<<<<<< .mine
 import React from "react";
 import BigAdvertWrapper from "../../components/BigAdvertWrapper";
-import {
-  StyledWrapper,
-  
-} from "./styles";
+import { StyledWrapper } from "./styles";
 import { MainHeader } from "../../components/MainHeader";
 
 class BigAdvert extends React.Component {
   render() {
     return (
       <StyledWrapper>
-        <BigAdvertWrapper></BigAdvertWrapper>
+        <BigAdvertWrapper />
       </StyledWrapper>
     );
   }
 }
 export default BigAdvert;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-import React from "react";
-import AdvertItem from "../../components/AdvertWrapper";
-import {
-  StyledWrapper,
-  StyledTitle,
-  StyledImg,
-  StyledDesc,
-  StyledPrice,
-  StyledUserInfo,
-  StyledUserName,
-  StyledUserNumber
-} from "./styles";
-import { MainHeader } from "../../components/MainHeader";
-
-class BigAdvert extends React.Component {
-  render() {
-    return (
-      <StyledWrapper>
-        <StyledTitle>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </StyledTitle>
-        <StyledImg />
-        <StyledDesc>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget
-          fringilla arcu, vel lobortis turpis. Vestibulum scelerisque vulputate
-          convallis. Integer quis mauris pretium, faucibus risus sed, egestas
-          purus. Curabitur at venenatis enim. Curabitur tempus, nibh vel aliquam
-          scelerisque, arcu dolor finibus ex, ut iaculis ex ex nec nunc. Morbi
-          consequat massa at ex blandit, eu posuere nisi euismod. Quisque
-          tincidunt, enim non auctor dictum, ligula dolor placerat risus, non
-          dignissim quam est non eros. Fusce euismod vehicula semper. Aenean
-          eleifend dui nec pretium interdum. Duis hendrerit orci ac erat
-          tincidunt gravida.
-        </StyledDesc>
-        <StyledUserInfo>
-          <StyledUserName>Ivan Hrozny</StyledUserName>
-          <StyledUserNumber>09123456</StyledUserNumber>
-        </StyledUserInfo>
-
-        <StyledPrice>Price: 100</StyledPrice>
-      </StyledWrapper>
-    );
-  }
-}
-export default BigAdvert;
-/* "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget
-          fringilla arcu, vel lobortis turpis. Vestibulum scelerisque vulputate
-          convallis. Integer quis mauris pretium, faucibus risus sed, egestas
-          purus. Curabitur at venenatis enim. Curabitur tempus, nibh vel aliquam
-          scelerisque, arcu dolor finibus ex, ut iaculis ex ex nec nunc. Morbi
-          consequat massa at ex blandit, eu posuere nisi euismod. Quisque
-          tincidunt, enim non auctor dictum, ligula dolor placerat risus, non
-          dignissim quam est non eros. Fusce euismod vehicula semper. Aenean
-          eleifend dui nec pretium interdum. Duis hendrerit orci ac erat
-          tincidunt gravida.", */
->>>>>>> .theirs
