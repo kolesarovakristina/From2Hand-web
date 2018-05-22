bc6874461efc0c94a48f746844b54d87e2d43298
Andrej <andrej.byra@centrum.sk>
2018-05-22 18:41:29 +0200

    admin can create category and upload photo

--- src/components/CategoryForm/index.js
+++ src/components/CategoryForm/index.js
@@ -6,7 +6,6 @@ import FormInput from '../FormInput';
 class CategoryForm extends React.Component {
 	state = {
 		title: '',
-		description: '',
 		photo: ''
 	};
 
@@ -28,16 +27,17 @@ class CategoryForm extends React.Component {
 	onSubmit = async (event) => {
 		event.preventDefault();
 		const form = new FormData();
-		form.append('title', `${this.state.title}`);
-		//form.append('description', `${this.state.description}`);
-		form.append('photo', `${this.state.photo}`);
+		form.append('photo', this.state.photo);
+		form.append('title', this.state.title);
+
 		try {
 			const response = await axios({
 				method: 'post',
 				url: '/category',
-				data: form,
-				config: { headers: { 'Content-Type': 'multipart/form-data' } }
+				mimeType: 'multipart/form-data',
+				data: form
 			});
+			console.log(form);
 		} catch (err) {
 			console.log(err);
 		}
@@ -47,8 +47,8 @@ class CategoryForm extends React.Component {
 		return (
 			<StyledWrapper>
 				<form onSubmit={this.onSubmit}>
-					<StyledInput type="text" onChange={this.handleTitle} />
-					<StyledInput type="text" onChange={this.handleDescription} />
+					<StyledInput type="text" onChange={this.handleTitle} onSubmit={this.onSubmit} />
+
 					<StyledInput
 						type="file"
 						onChange={this.handleSetFile}
