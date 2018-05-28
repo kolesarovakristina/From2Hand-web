433b8f6b4f86fc8c1a19eb18e58cafd0d3b3610e
Andrej <andrej.byra@centrum.sk>
2018-05-28 11:19:05 +0200

    added style for bigAdvert

--- src/containers/BigAdvert/index.js
+++ src/containers/BigAdvert/index.js
@@ -1,43 +1,55 @@
-import React from "react";
+import React from 'react';
+import img from '../../assets/horse.jpg';
 import {
-  StyledWrapper,
-  StyledTitle,
-  StyledImg,
-  StyledDesc,
-  StyledPrice,
-  StyledUserInfo,
-  StyledUserName,
-  StyledUserNumber
-} from "./styles";
+	StyledWrapper,
+	StyledTitle,
+	StyledImg,
+	StyledDesc,
+	StyledPrice,
+	StyledUserInfo,
+	StyledUserName,
+	StyledUserNumber,
+	StyledUserEmail,
+	StyledDescWrapper,
+	StyledLocation,
+	StyledDescTitle,
+	StyledLocationTitle,
+	StyledPriceTitle,
+	StyledContactTitle,
+	StyledNameTitle,
+	StyledNumberTitle,
+	StyledEmailTitle
+} from './styles';
 
 class BigAdvert extends React.Component {
-  render() {
-    return (
-      <StyledWrapper>
-        <StyledTitle>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </StyledTitle>
-        <StyledImg />
-        <StyledDesc>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget
-          fringilla arcu, vel lobortis turpis. Vestibulum scelerisque vulputate
-          convallis. Integer quis mauris pretium, faucibus risus sed, egestas
-          purus. Curabitur at venenatis enim. Curabitur tempus, nibh vel aliquam
-          scelerisque, arcu dolor finibus ex, ut iaculis ex ex nec nunc. Morbi
-          consequat massa at ex blandit, eu posuere nisi euismod. Quisque
-          tincidunt, enim non auctor dictum, ligula dolor placerat risus, non
-          dignissim quam est non eros. Fusce euismod vehicula semper. Aenean
-          eleifend dui nec pretium interdum. Duis hendrerit orci ac erat
-          tincidunt gravida.
-        </StyledDesc>
-        <StyledUserInfo>
-          <StyledUserName>Ivan Hrozny</StyledUserName>
-          <StyledUserNumber>09123456</StyledUserNumber>
-        </StyledUserInfo>
-
-        <StyledPrice>Price: 100</StyledPrice>
-      </StyledWrapper>
-    );
-  }
+	render() {
+		return (
+			<StyledWrapper>
+				<StyledTitle>Some Title</StyledTitle>
+				<StyledImg src={img} />
+				<StyledDescWrapper>
+					<StyledDescTitle>Description:</StyledDescTitle>
+					<StyledDesc>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget fringilla arcu, vel
+						lobortis turpis. Vestibulum scelerisque vulputate convallis. Integer quis mauris pretium,
+						faucibus risus sed, egestas purus.
+					</StyledDesc>
+					<StyledLocationTitle>Location:</StyledLocationTitle>
+					<StyledLocation>Košice 1, Kavečanyň</StyledLocation>
+					<StyledPriceTitle>Price:</StyledPriceTitle>
+					<StyledPrice>1000€</StyledPrice>
+				</StyledDescWrapper>
+				<StyledUserInfo>
+					<StyledContactTitle>CONTACT:</StyledContactTitle>
+					<StyledNameTitle>Name:</StyledNameTitle>
+					<StyledUserName>Ivan Hrozny</StyledUserName>
+					<StyledNumberTitle>Telephone Number:</StyledNumberTitle>
+					<StyledUserNumber>09123456</StyledUserNumber>
+					<StyledEmailTitle>Email address:</StyledEmailTitle>
+					<StyledUserEmail>ivako@mail.com</StyledUserEmail>
+				</StyledUserInfo>
+			</StyledWrapper>
+		);
+	}
 }
 export default BigAdvert;
--- src/containers/BigAdvert/styles.js
+++ src/containers/BigAdvert/styles.js
@@ -1,60 +1,119 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const StyledWrapper = styled.div`
-  // // display: flex;
-  // // flex-direction:column;
-  // //   flex-flow: column wrap;
-  // //   font-weight: bold;
-  // //
-  // height: 100%;
-  // width: 100%;
-  // background: lightgrey;
-  // //flex-direction: row;
-  position: absolute;
-  width: 100%;
-  height: 70%;
-`;
+export const StyledWrapper = styled.div``;
+
 export const StyledTitle = styled.h1`
-  background-color: rgb(22, 131, 147)
-  padding-top: 17px;
-  padding-left:15px;
+	background-color: #168393;
+	height: 55px;
+	width: 650px;
+	padding-top: 5px;
+	padding-left: 25px;
+	float: right;
+	margin-right: 150px;
+	color: white;
+	font-size: 40px;
+	border-radius: 10px;
+`;
+export const StyledDescWrapper = styled.div`
+	float: right;
+	width: 667px;
+	height: 230px;
+	margin-top: -26.5px;
+	margin-right: 150px;
+	border-radius: 10px;
+	border: 1.7px solid #168393;
+	padding-left: 5px;
+	background-color: white;
+`;
+export const StyledUserInfo = styled.div`
+	float: right;
+	margin-right: 150px;
+	font-size: 15px;
+	border-radius: 10px;
+	height: 180px;
+	width: 673px;
+	background-color: rgb(230, 230, 230);
 `;
 export const StyledImg = styled.img`
-  margin-left: 10px;
-  // padding-top: 112px;
-  // padding-left: 120px;
-  background: lightblue;
-  display: inline-block;
-  float: left;
-  width: 415px;
-  height: 314px;
+	margin-left: 85px;
+	margin-top: 90px;
+	float: left;
+	width: 415px;
+	height: 314px;
 `;
-export const StyledDesc = styled.span`
-  margin-right: 150px;
-  margin-left: 80px;
-  text-align: justify;
+export const StyledDesc = styled.div`
+	padding-left: 30px;
+	padding-right: 50px;
+	padding-top: 3px;
 `;
-export const StyledPrice = styled.span`
-  float: right;
-  padding-right: 10px;
-  padding-top: 35px;
-  margin-top: 225px;
-  font-size: 30px;
+export const StyledLocation = styled.div`
+	padding-left: 30px;
+	padding-top: 3px;
 `;
-export const StyledUserInfo = styled.span`
-  display: flex;
-  flex-direction: column;
-  float: left;
-  margin-top: 240px;
-  font-size: 30px;
-  border-style: solid;
-  // border-width: 5px;
+export const StyledPrice = styled.div`
+	padding-left: 30px;
+	padding-top: 3px;
 `;
-export const StyledUserName = styled.span`
-  margin-right: 150px;
-  margin-left: 80px;
+
+export const StyledUserName = styled.div`
+	margin-left: 30px;
+	padding-top: 4px;
 `;
-export const StyledUserNumber = styled.span`
-  margin-right: 150px;
-  margin-left: 80px;
+export const StyledUserNumber = styled.div`
+	margin-left: 30px;
+	padding-top: 4px;
+`;
+export const StyledUserEmail = styled.div`
+	margin-left: 30px;
+	padding-top: 4px;
+`;
+
+export const StyledDescTitle = styled.div`
+	font-size: 20px;
+	color: #494949;
+	font-weight: bold;
+	padding-top: 10px;
+	padding-left: 30px;
+`;
+export const StyledLocationTitle = styled.div`
+	font-size: 20px;
+	color: #494949;
+	font-weight: bold;
+	padding-top: 8px;
+	padding-left: 30px;
+`;
+export const StyledPriceTitle = styled.div`
+	font-size: 20px;
+	color: #494949;
+	font-weight: bold;
+	padding-left: 30px;
+	padding-top: 8px;
+`;
+export const StyledContactTitle = styled.div`
+	font-size: 20px;
+	color: #494949;
+	font-weight: bold;
+	margin-left: 30px;
+	padding-top: 5px;
+`;
+export const StyledNameTitle = styled.div`
+	font-size: 20px;
+	color: #494949;
+	font-weight: bold;
+	margin-left: 30px;
+	padding-top: 5px;
+`;
+export const StyledNumberTitle = styled.div`
+	font-size: 20px;
+	color: #494949;
+	font-weight: bold;
+	margin-left: 30px;
+	padding-top: 3px;
+`;
+export const StyledEmailTitle = styled.div`
+	font-size: 20px;
+	color: #494949;
+	font-weight: bold;
+	margin-left: 30px;
+	padding-top: 3px;
 `;
