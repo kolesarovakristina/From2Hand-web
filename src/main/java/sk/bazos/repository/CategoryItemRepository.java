ed3e0f03ab3ad80ec2ea10b8b9cf1ef95697e17e
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-06-03 18:02:02 +0200

    fixed commit

--- src/main/java/sk/bazos/repository/CategoryItemRepository.java
+++
@@ -1,7 +0,0 @@
-package sk.bazos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sk.bazos.model.CategoryItem;
-
-public interface CategoryItemRepository extends JpaRepository<CategoryItem, Long> {
-}
